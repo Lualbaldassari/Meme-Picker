8290a59b731c3bf6651951b749669a851f91cd2c
Lualbaldassari <lualbaldassari@gmail.com>
2023-03-17 17:56:15 -0500

    fix

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ function renderCat() {
   memeModalInner.innerHTML = `
         <img 
         class="cat-img" 
-        src="/images/${catObject.image}"
+        src="./images/${catObject.image}"
         alt="${catObject.alt}"
         >
         `;
